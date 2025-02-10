fc8802f4d8e7d25dec514e9291a232c5736ac40f
NicoRodri <nicorodriguez004@gmail.com>
2025-02-10 16:42:53 -0300

    MAXSCORE configurado a 10

--- src/dev/niko/Score.java
+++ src/dev/niko/Score.java
@@ -2,7 +2,7 @@ package dev.niko;
 
 public class Score {
 	public static int p1Score = 0, p2Score = 0;
-	public static int MAXSCORE = 8;
+	public static int MAXSCORE = 10;
 	public static int ultimoPunto = -1; //puede ser 1 o 2
 	public static int howMuchForTension = 3;
 	
